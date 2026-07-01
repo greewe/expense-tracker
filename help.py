67c2b8c18c5a7d57960045878d1af8761388bc2f
Abhinav Nair <dts1397@detasad.com>
2026-07-01 12:45:41 +0300

    expense tracker first version

---
+++ help.py
@@ -0,0 +1,9 @@
+
+def get_grade(score):
+    if score >=90 and score <=100:
+        return "A"
+    elif score >= 80:
+        return "B"
+    else:
+        return "C"
+
---
+++ project test.py
@@ -0,0 +1,23 @@
+import csv
+goal=25000
+expenses=[]
+with open('expenses.csv','r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        row['amount']=int(row['amount'])
+        expenses.append(row)
+print(expenses)
+
+category_totals = {}
+
+for e in expenses:
+    cat= e["category"]
+    amt= e["amount"]
+    if cat in category_totals:
+        category_totals[cat] = category_totals[cat] + amt
+    else:
+        category_totals[cat] = amt
+print(category_totals)
+u=sum(category_totals.values())
+print("grand value",u)
+print("remaining to earn", goal-u)
